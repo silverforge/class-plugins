d072de27512b928af9d2fe9883dbdf087ba94dfc
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2023-09-20 07:07:37 -0400

    host class types reintroduced

--- src/hosts/MyFirstHostClass.ts
+++ src/hosts/MyFirstHostClass.ts
@@ -2,6 +2,7 @@ import { PluginImpl1 } from "../plugins/PluginImpl1";
 import { PluginImpl2 } from "../plugins/PluginImpl2";
 import { BasePluginHostClass } from "./BasePluginHostClass";
 
+export type MyFirstHostClassType = MyFirstHostClass<[PluginImpl1, PluginImpl2]>;
 export default class MyFirstHostClass<P extends [PluginImpl1, PluginImpl2]> extends BasePluginHostClass<P> {
   get plugin1() {
     return this._plugins[0];
--- src/hosts/MySecondHostClass.ts
+++ src/hosts/MySecondHostClass.ts
@@ -3,6 +3,7 @@ import { PluginImpl2 } from "../plugins/PluginImpl2";
 import { PluginImpl3 } from "../plugins/PluginImpl3";
 import { BasePluginHostClass } from "./BasePluginHostClass";
 
+export type MySecondHostClassType = MySecondHostClass<[PluginImpl1, PluginImpl2, PluginImpl3]>;
 export default class MySecondHostClass<P extends [PluginImpl1, PluginImpl2, PluginImpl3]> extends BasePluginHostClass<P> {
   get plugin1() {
     return this._plugins[0];
